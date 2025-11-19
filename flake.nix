d2bf60783a79585a5bb29aff5083d2c435cb35c3
Tim Hollmann <mail@tim-hollmann.de>
2025-11-19 18:07:32 +0100

    Update cropping to improve Tesseract

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       pkgs.writeShellScriptBin "cafi-occupancy" ''
         set -e
         ${pkgs.wget}/bin/wget --quiet -O tmp.png "https://web.hochschulsport-hannover.de/campusfit/auslastungsgrafik"
-        ${pkgs.imagemagick}/bin/magick tmp.png -crop 800x240+330+150 tmp.png
+        ${pkgs.imagemagick}/bin/magick tmp.png -crop 800x240+300+150 tmp.png
         ${pkgs.imagemagick}/bin/magick tmp.png -fuzz 30% -fill transparent +opaque "#8DC63FFF" tmp.png
         ${pkgs.imagemagick}/bin/magick tmp.png -alpha off -threshold 50% -negate tmp.png
         ${pkgs.tesseract}/bin/tesseract --psm 8 tmp.png stdout nobatch digits
